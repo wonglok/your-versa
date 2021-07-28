cc620c64baf84d66f7cd334d4b2a234332969be6
wonglok <yellowhappy831@gmail.com>
2021-07-28 11:25:58 +0800

    save

--- pages/guide/setup-firebase.js
+++ pages/guide/setup-firebase.js
@@ -1,3 +1,20 @@
 export default function Step01() {
-  return <div className="w-full">Welcome to Setup</div>;
+  return (
+    <div className="w-full">
+      <div className="p-4 lg:p-12">
+        <div className="relative">
+          <div className="py-56 text-center bg-yellow-0">
+            <h1 className="text-3xl mb-3">Welcome to Setup Guide</h1>
+          </div>
+          <div>
+            <h1 className="text-2xl mb-3">Overview of Setup Setps</h1>
+            <div className="mb-3">1. Setup Firebase</div>
+          </div>
+          <div>
+            <div className="text-2xl mb-3">Setting up Firebase</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
