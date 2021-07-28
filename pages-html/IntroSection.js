8cd39e3e000cd27aa6089c62a2decb1b5ce1e151
wonglok <yellowhappy831@gmail.com>
2021-07-28 11:54:53 +0800

    wa

---
+++ pages-html/IntroSection.js
@@ -0,0 +1,8 @@
+export function IntroSection() {
+  //
+  return (
+    <div className={"p-4 lg:p-12 text-center"}>
+      <img className="inline-block" src="/img/metaverse.png"></img>
+    </div>
+  );
+}
